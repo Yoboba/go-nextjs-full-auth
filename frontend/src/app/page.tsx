d1eb3a0805b6da6f9b95a2479baadba27b69f5c3
Yobubble <works.bee.onl@gmail.com>
2024-01-10 23:13:30 +0700

    Sign in page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from 'next/image'
 import { Button } from '@/components/ui/button'
+import Landing from '@/components/sign-in/landing'
 export default function Home() {
+
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className='flex h-full justify-center items-center'>
+      <div className="w-[480px] h-[480px] m-16 bg-white rounded-2xl flex justify-center items-center">
+        <Landing/>
+      </div>
     </div>
   )
 }
