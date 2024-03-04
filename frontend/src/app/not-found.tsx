385c1fb8558e8f9758492267a9fa5e9a2699d2eb
Yobubble <thanachot.onl@student.mahidol.edu>
2024-03-04 19:40:04 +0700

    feat: not_found_page

---
+++ frontend/src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFoundPage() {
+    return (
+        <main className="bg-white h-screen flex flex-col justify-center items-center gap-4">
+            <h1 className=" bg-gradient-to-r from-g2 to-g4 text-transparent bg-clip-text w-fit font-bold text-[200px] ">404</h1>
+            <h1 className=" text-4xl text-g1">PAGE NOT FOUND</h1>
+        </main>
+    )
+}
