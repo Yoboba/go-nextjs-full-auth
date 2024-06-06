3e3814af128ab56a3fb7fa2259f203d7c9c6f164
Yobubble <thanachot.onl@student.mahidol.edu>
2024-06-06 20:25:13 +0700

    refactor : Readme.md

--- README.md
+++ README.md
@@ -1,26 +1,61 @@
-# Simple Blog Web Application (Medium.com inspired)
+# Blog Web Application (Block)
+A simple blog web application inspired by [Medium](https://medium.com).
 
-Tech Stack
-- Languages Used : GO, Typescript
-- Frontend Frameworks : React.js, Next.js, Shadcn/ui, TailwindCSS
-- Backend Frameworks : GO Fiber, GORM
-- Database : Postgres (docker)
+## Tech Stack
+- **Languages Used:** GO, Typescript
+- **Frontend Frameworks:** React.js, Next.js, Shadcn/ui, TailwindCSS
+- **Backend Frameworks:** GO Fiber, GORM
+- **Database:** Postgres (Docker)
 
-Project structure : Clean Architecture
+### Project Structure
+- **Architecture:** Clean Architecture
 
-Configuration step
-- Run docker compose to open mock PostgreSQL server
-- .env file (JWT_SECRET, sender email, google app password)
-- server config.yaml
+## Setup Instructions
+1. **Run Docker Compose** to start the mock PostgreSQL server.
+2. **Customize server and database configuration** (optional):
 
-#### Setup Instruction
-1. Clone
-```bash
-git clone https://github.com/Yoboba/go-nextjs-full-auth.git
-```
-2. dependencies installation
-```bash
-cd frontend
-npm install
-```
-3. Setup environment variable for PostgreSQL and PgMyAdmin
+    ```yaml
+    app:
+      server:
+        port: 7070  # server port
+
+    database:
+      host: localhost
+      port: 5432
+      user: myuser  # database user
+      password: mypassword  # database password
+      dbname: TEST  # database name
+      sslmode: disable
+    ```
+
+3. **Configure server environment variables** in `.env`:
+    - `JWT_SECRET`
+    - `SENDER_EMAIL`
+    - `GOOGLE_APP_PASSWORD`
+4. **Customize mock data** in the migrations folder (note that this is just mock data).
+
+### API Documentation
+Find the API documentation on [Postman](https://www.postman.com/altimetry-cosmologist-63308896/workspace/yobubble-s-playground/collection/32957457-a675066f-9852-472c-96e1-dcf85c82791b?action=share&creator=32957457).
+
+## Running the Application
+1. Ensure the PostgreSQL container is running.
+2. **Backend:**
+    ```sh
+    cd backend
+    go run cmd/block/block.go
+    ```
+3. **Frontend:**
+    ```sh
+    cd frontend
+    npm install
+    npm run dev
+    ```
+
+## Features Included
+- Create, Read, Update, Delete Blog
+- Real-time like count and status
+- Sign In, Sign Up with JWT for Authentication and Authorization
+- Password Reset (Email Verification Code)
+- Profile Search
+- Tag Search
+- Profile Image Change
--- backend/pkg/migrations/mock_data.go
+++ backend/pkg/migrations/mock_data.go
@@ -14,36 +14,36 @@ var roles = []entities.Role{
 }
 var users = []entities.User{
 	{
-		Username: "Yobubble",
-		Email:    "thanachot.onlamoon@gmail.com", // TODO : will need to delete this since its the real email
-		Password: "$2a$10$DdB6INzHJegaH6FP3cAoN.gyYWhsf5FteT2lcpNa0pmCr3QtP2LK.",
+		Username: "Cyan",
+		Email:    "cyan@gmail.com",                                               // <- email need to be lowercase
+		Password: "$2a$10$eDO6.zcFdV5hYkJhb1/aYuWUeKk4d/hrONTLH2RACbaL.7EHEyh/S", // bcrypt, cost = 10
 		RoleID:   1,
-	}, // password : yobuza007
+	}, // password : QR#&qm
 	{
-		Username: "Prisma",
-		Email:    "nanthapat.wat@gmail.com",
-		Password: "$2a$10$PnRt9tWH93bC/95k5510m.fFeARX8XqvDVsVeNOYzDbQ7F/brz3ii",
+		Username: "Orange",
+		Email:    "orange@gmail.com",                                             // <- email need to be lowercase
+		Password: "$2a$10$JfeAcnVMUo5dJhG3L4hm3eNfukyX8v8UMu6Wsmn9DamPt1JXKaJTG", // bcrypt, cost = 10
 		RoleID:   2,
-	}, // password : guideza007
+	}, // password : ipbPS$
 	{
-		Username: "DEVN",
-		Email:    "phoonawit.ler@gmail.com",
-		Password: "$2a$10$ojwVltOCkQkbKzxO1UGt8OJhKqhPMvRVIYsVJPg3niqK.b8uM.UA2",
+		Username: "Blue",
+		Email:    "blue@gmail.com",                                               // <- email need to be lowercase
+		Password: "$2a$10$770W822AY3ayav4H28fND.I0KaZIlqGGLi1XaqklwX50lflmAQY3G", // bcrypt, cost = 10
 		RoleID:   1,
-	}, // password : devnza007
+	}, // password : QCR^2n
 	{
-		Username: "Batman8K",
-		Email:    "sanhanut.nig@gmail.com",
-		Password: "$2a$10$FpwJ7b0p3bwtaE3gpvp/huyjGEPI9bq1/rjJobQvA83R6sawxl1DG",
+		Username: "White",
+		Email:    "white@gmail.com",                                              // <- email need to be lowercase
+		Password: "$2a$10$N8MjaMS3nG9QA8HHFDNU/e4q/9D674koABnMJCTIb8az38xHaklMa", // bcrypt, cost = 10
 		RoleID:   1,
-	}, // password : batman8kza007
+	}, // password : MMnf5A
 }
 var blogs = []entities.Blog{
 	{
 		Title:   "Building Scalable Microservices with Go",
 		Caption: "Discover how the Go programming language is revolutionizing the development of scalable, efficient microservices.",
 		Body:    "The Go programming language, often referred to as Golang, has become a popular choice for developing microservices. Its simplicity, performance, and strong support for concurrent programming make it an ideal fit for building scalable and efficient microservices architectures.",
-		UserID:  1, // <- Yobubble
+		UserID:  1, // <- Cyan
 		Tags: []entities.Tag{
 			{
 				Name: "GO",
@@ -66,7 +66,7 @@ var blogs = []entities.Blog{
 		Title:   "Implementing Clean Architecture in Go: Best Practices and Benefits",
 		Caption: "Learn how to leverage the principles of clean architecture in Go to build maintainable, scalable, and testable applications.",
 		Body:    "Clean architecture is a design philosophy that aims to create systems that are easy to maintain, test, and scale by organizing code into layers with distinct responsibilities. When implemented in Go, clean architecture can help developers build robust applications that stand the test of time and adapt to changing requirements.",
-		UserID:  2, // <- Prisma
+		UserID:  2, // <- Orange
 		Tags: []entities.Tag{
 			{
 				Name: "GO", // <- Duplicated data
@@ -86,7 +86,7 @@ var blogs = []entities.Blog{
 		Title:   "The Evolution of Integrated Development Environments",
 		Caption: "Explore the fascinating journey of Integrated Development Environments (IDEs) and how AI-powered features are transforming the way developers write code.",
 		Body:    "Integrated Development Environments (IDEs) have come a long way since their inception. What started as simple text editors has evolved into sophisticated platforms that significantly enhance developer productivity. This article explores the history of IDEs, their current state, and the exciting future trends driven by artificial intelligence.",
-		UserID:  4, // <- Batman8K
+		UserID:  4, // <- White
 		Tags: []entities.Tag{
 			{
 				Name: "IDE",
@@ -106,7 +106,7 @@ var blogs = []entities.Blog{
 		Title:   "The Rise of Chinese Language Processing",
 		Caption: "Explore how advancements in Chinese language processing are revolutionizing AI and technology, opening up new possibilities for innovation and communication.",
 		Body:    "The Chinese language, with its rich history and complex structure, presents unique challenges and opportunities for the field of natural language processing (NLP). As AI continues to advance, breakthroughs in Chinese language processing are unlocking new possibilities for innovation and communication. This article delves into the importance of Chinese language processing, recent advancements, and their implications for AI and technology.",
-		UserID:  1, // <- Yobubble
+		UserID:  1, // <- Cyan
 		Tags: []entities.Tag{
 			{
 				Name: "Chinese Language Processing",
@@ -126,7 +126,7 @@ var blogs = []entities.Blog{
 		Title:   "The Art of Ikigai",
 		Caption: "Explore the Japanese concept of Ikigai, a philosophy that encourages living a balanced and fulfilling life by finding your true purpose.",
 		Body:    "In a fast-paced and often stressful world, the Japanese concept of Ikigai offers a refreshing perspective on life and fulfillment. Rooted in Japanese culture, Ikigai is a philosophy that encourages individuals to find their purpose and live a balanced, meaningful life. This article delves into the essence of Ikigai, its cultural significance, and how you can incorporate its principles into your daily life.",
-		UserID:  1, // <- Yobubble
+		UserID:  1, // <- Cyan
 		Tags: []entities.Tag{
 			{
 				Name: "Ikigai",
